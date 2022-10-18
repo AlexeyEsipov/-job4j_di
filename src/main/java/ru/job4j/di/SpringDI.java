82bb08d0791a15048504af5952a8ac3289b38e1f
Esipov Aleksey <a_esipov@list.ru>
2022-10-18 11:33:01 +0300

    0. Что такое DI? [#6876]

---
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -0,0 +1,20 @@
+package ru.job4j.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(StartUI.class);
+        context.register(ConsoleInput.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+
+        ConsoleInput ci = context.getBean(ConsoleInput.class);
+        ci.askStr("Hi, please enter your name:");
+    }
+}
